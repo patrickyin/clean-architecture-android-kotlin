7399d8f8649d1112d75fa29805fc78d6b6e28f86
Patrick Yin <eetech@gmail.com>
2017-05-23 21:01:02 +1000

    add clean activity and presenter

---
+++ mvp/src/main/java/io/github/eetech/cleanarchitecture/mvp/CleanPresenter.kt
@@ -0,0 +1,24 @@
+package io.github.eetech.cleanarchitecture.mvp
+
+import android.support.v4.app.Fragment
+import android.support.v7.app.AppCompatActivity
+import io.reactivex.disposables.CompositeDisposable
+
+abstract class CleanPresenter<out T> {
+  private var view: T? = null
+
+  abstract fun initialise()
+  abstract fun disposeSubscriptions()
+
+  fun getView(): T? = view
+
+  @Suppress("UNCHECKED_CAST")
+  fun attachView(view: Any?) {
+    this.view = view as T?
+  }
+
+  fun detachView() {
+    view = null
+  }
+}
+
