5edf003a9b561cc39de5fc800fd6a8482365ace5
Patrick Yin <eetech@gmail.com>
2018-04-16 23:04:48 +1000

    Optimize code

--- ui/src/main/java/io/github/patrickyin/cleanarchitecture/app/App.kt
+++ ui/src/main/java/io/github/patrickyin/cleanarchitecture/app/App.kt
@@ -4,17 +4,11 @@ import android.app.Application
 import io.github.patrickyin.cleanarchitecture.app.di.component.AppComponent
 import io.github.patrickyin.cleanarchitecture.app.di.component.DaggerAppComponent
 import io.github.patrickyin.cleanarchitecture.app.di.module.AppModule
-import io.github.patrickyin.cleanarchitecture.app.di.module.IOModule
-import io.github.patrickyin.cleanarchitecture.app.di.module.RepositoryModule
-import io.github.patrickyin.cleanarchitecture.app.di.module.UseCaseModule
 
 class App : Application() {
   val applicationComponent: AppComponent by lazy {
     DaggerAppComponent.builder()
       .appModule(AppModule(this))
-      .iOModule(IOModule())
-      .repositoryModule(RepositoryModule())
-      .useCaseModule(UseCaseModule())
       .build()
   }
 
